3d8acb54cb69f6f7e3079c66131a596cdbc6be70
bc2024-3 <bzabrodskij20@gmail.com>
2024-10-16 19:19:14 +0300

    add new

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
+
+// Налаштування командної програми
+program
+  .requiredOption('-i, --input <path>', 'Input file path')
+  .option('-o, --output <path>', 'Output file path')
+  .option('-d, --display', 'Display the result in console')
+  .parse(process.argv);
+
+// Отримання аргументів
+const options = program.opts();
+
+// Функція для читання файлу
+function readInputFile(filePath) {
+  if (!fs.existsSync(filePath)) {
+    console.error('Cannot find input file');
+    process.exit(1);
+  }
+  return fs.readFileSync(filePath, 'utf8');
+}
+
+// Функція для виводу результату
+function writeOutputFile(outputPath, data) {
+  fs.writeFileSync(outputPath, data, 'utf8');
+}
+
+// Основна логіка програми
+try {
+  const inputData = readInputFile(options.input);
+
+  if (options.display) {
+    console.log(inputData);
+  }
+
+  if (options.output) {
+    writeOutputFile(options.output, inputData);
+  }
+
+  // Якщо не задано -o або -d, програма нічого не виводить
+  if (!options.output && !options.display) {
+    process.exit(0);
+  }
+} catch (err) {
+  console.error(err.message);
+  process.exit(1);
+}
